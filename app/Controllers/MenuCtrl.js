e318291fa8339c5dca09c83abda2cb279e598079
soote <waffleee@gmail.com>
2013-12-19 15:53:22 -0500

    not using awesome-font

--- app/Controllers/MenuCtrl.js
+++ app/Controllers/MenuCtrl.js
@@ -1,7 +1,9 @@
 Grades.controller('MenuCtrl', function ($scope, FormatCourses) {
-	$scope.menuLinks = [{link:"home",text:"home"},
-						{link:"details",text:"Profile Details"},
-						{link:"started",text:"Getting Started"}];
+	$scope.menuLinks = [
+		{link:"home",text:"home"},
+		{link:"details",text:"Profile Details"},
+		{link:"started",text:"Getting Started"}
+	];
 
  	$scope.selectedIndex = 0; // Whatever the default selected index is, use -1 for no selection
 
