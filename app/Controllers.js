3cc9190028da5d38a033d472bc56ee7458b5b30f
soote <waffleee@gmail.com>
2013-12-19 15:48:46 -0500

    erase duplicate pages

--- app/Controllers.js
+++
@@ -1,8 +0,0 @@
-Grades.controller('CourseCtrl', function ($scope, FormatCourses) {
-	$scope.onSubmit = function() {
-		$scope.courses = FormatCourses.format($scope.courseListInput);
-	}
-	$scope.setHighlight = function() {
-		$scope.selected = '.current';
-	}
-})
