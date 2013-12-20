b7a894a0005ce7dd9e38a71e2ecdf51efc0e1aba
soote <waffleee@gmail.com>
2013-12-19 20:30:31 -0500

    user registration in progress

---
+++ php/common.php
@@ -0,0 +1,11 @@
+<?php
+
+$SERVER   = "localhost";
+$USERNAME = "dn09uo";
+$PASSWORD = "8Ns6qADAvuPr8sss";
+$DATABASE = "dn09uo";
+
+$db = new PDO("mysql:dbname={$DATABASE}; host={$SERVER}", $USERNAME, $PASSWORD);
+$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+?>
