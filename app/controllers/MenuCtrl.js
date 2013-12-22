d6bb65b4062fe4bc3edead0c6a7c19d01a6ae814
soote <waffleee@gmail.com>
2013-12-22 00:42:46 -0500

    css changes

--- app/controllers/MenuCtrl.js
+++ app/controllers/MenuCtrl.js
@@ -1,8 +1,8 @@
 Grades.controller('MenuCtrl', function ($scope, FormatCourses) {
 	$scope.menuLinks = [
-		{link:"home",text:"home"},
-		{link:"details",text:"Profile Details"},
-		{link:"started",text:"Getting Started"}
+		{link:"home",text:"Home",icon:"home"},
+		{link:"details",text:"Profile Details",icon:"info"},
+		{link:"started",text:"Getting Started",icon:"question"}
 	];
 
  	$scope.selectedIndex = 0;
